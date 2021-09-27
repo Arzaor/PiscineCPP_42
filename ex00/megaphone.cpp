9181066820a510d5014d34e4e9d3ecff096e04da
Jeremy <zyrtec06200@live.fr>
2021-09-28 00:46:50 +0200

    Add ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarette <jbarette@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/28 00:33:40 by jbarette          #+#    #+#             */
+/*   Updated: 2021/09/28 00:46:10 by jbarette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	int	i, k;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (argv[i])
+		{
+			k = 0;
+			while (argv[i][k])
+				printf("%c", toupper(argv[i][k++]));
+			i++;
+		}
+	}
+	else
+		printf("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+	return 0;
+}
