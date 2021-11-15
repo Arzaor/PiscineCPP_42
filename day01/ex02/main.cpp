4bae514cd2a8ae21fa169bd62db5521270db7b65
jeremybarette <jeremy.barette@outlook.fr>
2021-11-15 20:58:19 +0100

    EX02 finish

---
+++ day01/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarette <jbarette@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 20:50:51 by jbarette          #+#    #+#             */
+/*   Updated: 2021/11/15 20:57:33 by jbarette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+
+	std::string* stringPTR = &string;
+	std::string& stringREF = string;
+	std::cout << &string << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
