b0563a66d7a3613e9ff3175ae470015ee2632e82
BARETTE <zyrtec06200@live.fr>
2022-01-04 18:38:50 +0100

    DAY05/EX01 finish

---
+++ day05/ex01/Bureaucrat.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#ifndef BUREAUCRAT_H
+
+#include <iostream>
+#include <string>
+#include "Form.hpp"
+
+class Bureaucrat
+{
+    public:
+        Bureaucrat(const std::string _name, int _grade);
+        Bureaucrat(const Bureaucrat & rhs);
+        ~Bureaucrat();
+
+        Bureaucrat &operator=(const Bureaucrat & rhs);
+
+        void        incrementGrade();
+        void        decrementGrade();
+        void        signForm(Form & rhs);
+
+        std::string getName()   const;
+        int         getGrade()  const;
+
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw()
+                {
+                    return ("Level is too low.");
+                }
+        };
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw()
+                {
+                    return ("Level is too high.");
+                }
+        };
+    private:
+        const std::string   _name;
+        int                 _grade;
+
+};
+
+std::ostream&	operator<<(std::ostream& ofs, Bureaucrat const & rhs);
+
+#endif
